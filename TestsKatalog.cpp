20121c0aa26887958e167907f0a556b02dc7963d
MoloZzz <99015088+MoloZzz@users.noreply.github.com>
2022-07-28 17:28:42 +0300

    Second
    Linked list and defolt tree with 2 branches(every node have only 2 chield)

--- TestsKatalog.cpp
+++ TestsKatalog.cpp
@@ -1,4 +1,4 @@
-﻿#include<iostream>
+#include<iostream>
 
 using namespace std;
 
@@ -139,6 +139,7 @@ void get_node_by_position_linked_list(node *head,int position) {
 	}
 
 }
+
 void test_linked_list() {
 	node* head = new node;
 	head = NULL;
@@ -167,10 +168,100 @@ void test_linked_list() {
 	get_node_by_position_linked_list(head, 4);
 }
 
+struct TreeNode {
+	int data;
+	TreeNode* r, * l;
+};
+void create_binary_tree(TreeNode *&root, int data) {
+	if (root != NULL) {
+		cout << "Tree is already created\n";
+		return;
+	}
+	else {
+		TreeNode* newTreeNode = new TreeNode;
+		newTreeNode->data = data;
+		newTreeNode->r = nullptr;
+		newTreeNode->l = nullptr;
+		root = newTreeNode;
+	}
+}
+
+void print_binary_tree(TreeNode* root,int layer) {
+	if (root == nullptr && layer == 1) {
+		cout << "There is not root\n create tree firstly\n";
+		return;
+	}else if (root == nullptr) {
+		return;
+	}else {
+		cout << root->data << endl;
+		cout << "Layer: " << layer << endl;
+		layer++;
+		if (root->r != nullptr)
+			print_binary_tree(root->r,layer);
+		if (root->r != nullptr)
+			print_binary_tree(root->l,layer);
+
+	}
+}
+
+void add_branch_binary_tree(TreeNode* root, int data) {
+	if (root == NULL) {
+		cout << "You have not root\n";
+		return;
+	}
+	else {
+		TreeNode* newTreeNode = new TreeNode;
+		newTreeNode->data = data;
+		newTreeNode->r = nullptr;
+		newTreeNode->l = nullptr;
+		if (root->r == NULL) {
+			root->r = newTreeNode;
+			return;
+		}
+		else if (root->l == NULL) {
+			root->l = newTreeNode;
+			return;
+		}
+		else {
+
+			if (root->r->r == NULL || root->r->l == NULL)
+			{
+				add_branch_binary_tree(root->r, data);
+			}
+			else if (root->l->r == NULL || root->l->l == NULL)
+			{
+				add_branch_binary_tree(root->l, data);
+			}
+			else {
+				add_branch_binary_tree(root->r, data);
+			}
+
+
+
+		}
+
+	}
+}
+
+void test_binary_tree() {
+	TreeNode* root = new TreeNode;
+	root = NULL;
+	create_binary_tree(root, 15);
+	add_branch_binary_tree(root, 16);
+	add_branch_binary_tree(root, 17);
+	add_branch_binary_tree(root, 8);
+	add_branch_binary_tree(root, 1);
+	add_branch_binary_tree(root, 10);
+	add_branch_binary_tree(root, 1123);
+	add_branch_binary_tree(root, 112);
+	add_branch_binary_tree(root, 155);
+	add_branch_binary_tree(root, 182);
+	print_binary_tree(root,1);
+}
+
 int main() {
 setlocale(LC_ALL, "ru");
-
-test_linked_list();
-
+// test_linked_list();
+// test_binary_tree();
 
 }
